526041cb11297eb577ca27f1ee39c0817f70ed84
Drew Gingerich <gingerich93@gmail.com>
2020-08-14 17:58:50 -0700

    Add player controls stub

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+	private PlayerInput input;
+	private Vector2 mousePosition = Vector2.zero;
+	private bool interact = false;
+	private bool turn = false;
+
+	public void OnAim(InputAction.CallbackContext ctx)
+	{
+		mousePosition = ctx.ReadValue<Vector2>();
+	}
+
+	public void OnInteract(InputAction.CallbackContext ctx)
+	{
+		interact = true;
+	}
+
+	public void OnTurn(InputAction.CallbackContext ctx)
+	{
+		turn = true;
+	}
+}
