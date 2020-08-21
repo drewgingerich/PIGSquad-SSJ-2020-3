90f2e0c0cd56b220b56dfb808dd587e422df4daf
drewgingerich <gingerich93@gmail.com>
2020-08-20 21:55:28 -0700

    Make script to size quads to texture

---
+++ Assets/Objects/SizeToTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SizeToTexture : MonoBehaviour
+{
+	public int pixPerUnit = 200;
+	public Texture2D texture;
+
+	void OnEnable()
+	{
+		SetSize();
+	}
+
+	private void SetSize()
+	{
+		if (texture == null) return;
+		transform.localScale = new Vector3(texture.width / pixPerUnit, texture.height / pixPerUnit, 1);
+	}
+}
