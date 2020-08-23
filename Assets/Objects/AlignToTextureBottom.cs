0c2ddb61d4e2cb20ec0c51cfc95928f97fc462f6
drewgingerich <gingerich93@gmail.com>
2020-08-22 18:42:38 -0700

    Add script to align object bottoms at zero

---
+++ Assets/Objects/AlignToTextureBottom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class AlignToTextureBottom : MonoBehaviour
+{
+	public int pixPerUnit = 200;
+	public Texture2D texture;
+
+	private void Awake()
+	{
+		Align();
+	}
+
+#if UNITY_EDITOR
+	private void Update()
+	{
+		Align();
+	}
+#endif
+
+	private void Align()
+	{
+		if (texture == null) return;
+		var unitHeight = texture.height / pixPerUnit;
+		transform.localPosition = Vector3.up * unitHeight * 0.5f;
+	}
+}
