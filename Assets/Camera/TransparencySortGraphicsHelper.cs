6f07d4063de81d379efe4fc977c24b66ea20b4f2
drewgingerich <gingerich93@gmail.com>
2020-08-25 16:14:59 -0700

    Mess with transparency sort settings

---
+++ Assets/Camera/TransparencySortGraphicsHelper.cs
@@ -0,0 +1,30 @@
+﻿// https://forum.unity.com/threads/transparency-sort-mode-and-lightweight-render-pipeline.651700/
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+class TransparencySortGraphicsHelper
+{
+	static TransparencySortGraphicsHelper()
+	{
+		OnLoad();
+	}
+
+	[RuntimeInitializeOnLoadMethod]
+	static void OnLoad()
+	{
+		GraphicsSettings.transparencySortMode = TransparencySortMode.CustomAxis;
+		GraphicsSettings.transparencySortAxis = new Vector3(0, 0, 1f);
+		// GraphicsSettings.transparencySortMode = TransparencySortMode.Orthographic;
+		Debug.Log(GraphicsSettings.transparencySortMode);
+		Debug.Log(GraphicsSettings.transparencySortAxis);
+		// Debug.Log("Graphics settings: transparency sort mode set to use Z-axis");
+	}
+}
